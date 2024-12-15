87da43a4de46be12402ebfff09be760c17bd8e50
LChansson <love.hansson@gmail.com>
2024-12-15 21:26:06 +0100

    FUrther cleanup of sim grid code

--- api/sim_grid_refactor.R
+++ api/sim_grid_refactor.R
@@ -9,34 +9,27 @@ create_sim_grid <- function(case) {
   
   ## Pre-processing ----
   
-  case$number_sims <- 1
-  
-  # Only do pre-processing once per case
+  # calculations
   case$number_of_operating_days <- case$vehicle_service_life * case$run_days_per_week * (52 - case$holiday_weeks_per_year)
+  case$largest_battery_size <- case$electricity_per_10km / 10 * case$longer_driving_distance
+  case$total_home_charging <- as.numeric(case$night_charging) + as.numeric(case$day_extra_home_charging)
   # case$battery_cost <- (case$bev_truck_cost - case$diesel_truck_cost) / case$battery_size
   
+  # unit conversion
   case$diesel_conversion_ratio <- case$diesel_per_10km / case$electricity_per_10km
-  
-  case$largest_battery_size <- case$electricity_per_10km / 10 * case$longer_driving_distance
-  
-  # case$charger_utilisation <- case$night_charging_capacity
-  
-  # mil to km
   case$shorter_driving_distance <- case$shorter_driving_distance * 10
   case$longer_driving_distance <- case$longer_driving_distance * 10
   
-
+  
+  
   ## Build sim grid ----
   
   all_vars <- c(
     "diesel_cost",
     "private_charging_cost",
     "public_charging_cost",
-    "battery_cost",
     "charger_cost",
     "vehicle_service_life",
-    "time_at_charger",
-    "optimal_charging_rate",
     "number_of_operating_days",
     "shorter_driving_distance",
     "longer_driving_distance"
