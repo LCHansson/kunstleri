0aba4cc93fb53804e2df4c34b8ee0eda95c79f39
LChansson <love.hansson@gmail.com>
2024-12-15 21:25:47 +0100

    Start work on refined total TCO calculations

--- api/profitability_simulation.R
+++ api/profitability_simulation.R
@@ -1,5 +1,5 @@
 
-profitability_simulation <- function(case) {
+profitability_simulation <- function(case, additional_params) {
   ## Check prereqs
   if (is.null(case$sim_grid) || is.null(case$dists))
     stop()
@@ -12,15 +12,6 @@ profitability_simulation <- function(case) {
   ## Calculate totals ----
   if (all(c("include_private_charging", "include_public_charging") %in% case$charge_modes)) {
     # Both charging modes (standard case)
-    # case$sim_grid <- case$sim_grid |> 
-    #   mutate(
-    #     total_private_electricity_consumption = case$dist_model$area_below_battery * number_of_operating_days * case$electricity_per_10km / 10,
-    #     total_public_electricity_consumption = case$dist_model$area_above_battery * number_of_operating_days * case$electricity_per_10km / 10,
-    #     total_private_electricity_cost = total_private_electricity_consumption * private_charging_cost,
-    #     total_public_electricity_cost = total_public_electricity_consumption * public_charging_cost,
-    #     total_diesel_consumption = case$dist_model$area_total * number_of_operating_days * case$diesel_per_10km / 10,
-    #     total_diesel_cost = total_diesel_consumption * diesel_cost
-    #   )
     case$sim_grid$total_private_electricity_consumption = case$dist_model$area_below_battery * case$sim_grid$number_of_operating_days * case$electricity_per_10km / 10
     case$sim_grid$total_public_electricity_consumption = case$dist_model$area_above_battery * case$sim_grid$number_of_operating_days * case$electricity_per_10km / 10
     case$sim_grid$total_private_electricity_cost = case$sim_grid$total_private_electricity_consumption * case$sim_grid$private_charging_cost
@@ -66,17 +57,20 @@ profitability_simulation <- function(case) {
   }
   
   ## Profitability ----
-  # case$sim_grid <- case$sim_grid |> 
-  #   mutate(
-  #     bev_total_tco = case$bev_truck_cost + case$charger_utilisation * charger_cost + total_private_electricity_cost + total_public_electricity_cost,
-  #     ice_total_tco = case$diesel_truck_cost + total_diesel_cost,
-  #     bev_is_profitable = bev_total_tco < ice_total_tco
-  #   )
-  case$sim_grid$bev_total_tco = case$bev_truck_cost + case$charger_utilisation * case$sim_grid$charger_cost + case$sim_grid$total_private_electricity_cost + case$sim_grid$total_public_electricity_cost
-  case$sim_grid$ice_total_tco = case$diesel_truck_cost + case$sim_grid$total_diesel_cost
-  case$sim_grid$bev_is_profitable = case$sim_grid$bev_total_tco < case$sim_grid$ice_total_tco
   
-  # case$share_profitable_in_sim <- sum(case$sim_grid$bev_is_profitable) / nrow(case$sim_grid)
+  additional_ice_costs <- 0
+  additional_bev_costs <- 0
+  
+  case$sim_grid$bev_total_tco = case$bev_truck_cost - case$bev_climate_premium +
+    (case$charger_cost + case$grid_cost) / case$charger_sharing_n +
+    case$sim_grid$total_private_electricity_cost +
+    case$sim_grid$total_public_electricity_cost +
+    additional_bev_costs
+  case$sim_grid$ice_total_tco = case$diesel_truck_cost + 
+    case$sim_grid$total_diesel_cost +
+    additional_ice_costs
+  
+  case$sim_grid$bev_is_profitable = case$sim_grid$bev_total_tco < case$sim_grid$ice_total_tco
   
   case
 }
