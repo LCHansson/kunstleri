f4e30887fc98372b7b948709435d72ecd312bd2b
Jonathan Hise Kaldma <info@hisekaldma.com>
2024-12-20 16:16:10 +0100

    Redesign cost warning to be more tip-like

--- server.R
+++ server.R
@@ -649,18 +649,24 @@ server <- function(input, output, session) {
       case <- CaseData()
       
       if (!is.null(case$warning))
-        display_warning <- "block"
+        display_warning <- "flex"
       else
         display_warning <- "none"
       
       div(
-        class = "calculator-element-block",
+        style = glue::glue("display: {display_warning};"),
+        class = "cost-warning",
         div(
-          style = glue::glue("display: {display_warning};"),
-          class = "cost-warning",
-          "Lorem odio velit aliquet quis est euismod sollicitudin ridiculus dis facilisi id
-enim fermentum? Id magnis velit aliquet rhoncus justo.",
-          tooltip(tags$strong("Läs eventuellt mer här"), "Det här kanske blir en text om varför du fått en varning")
+          class = "icon-wrapper",
+          HTML(r"(<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M272 384c9.6-31.9 29.5-59.1 49.2-86.2c0 0 0 0 0 0c5.2-7.1 10.4-14.2 15.4-21.4c19.8-28.5 31.4-63 31.4-100.3C368 78.8 289.2 0 192 0S16 78.8 16 176c0 37.3 11.6 71.9 31.4 100.3c5 7.2 10.2 14.3 15.4 21.4c0 0 0 0 0 0c19.8 27.1 39.7 54.4 49.2 86.2l160 0zM192 512c44.2 0 80-35.8 80-80l0-16-160 0 0 16c0 44.2 35.8 80 80 80zM112 176c0 8.8-7.2 16-16 16s-16-7.2-16-16c0-61.9 50.1-112 112-112c8.8 0 16 7.2 16 16s-7.2 16-16 16c-44.2 0-80 35.8-80 80z"/></svg>)")
+        ),
+        div(
+          class = "cost-warning-text",
+          p(
+            class = "cost-warning-header",
+            "Tips! Överväg ett större batteri"
+          ),
+          p("Merkostnaden kan vara mycket mindre än kostnaden för att stödladda bilen.")
         )
       )
     })
